eb73e9b44fca8bc17bf5e3980cc1c751eb1bc331
Tomer Amir <tomer@apiiro.com>
2025-01-28 15:30:35 +0200

    demo2

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ process.on('SIGINT', () => process.exit(0));
 
 app.get('/', (req, res) => res.send('success!'));
 app.get('/hi', (req, res) => res.send('hi!'));
+app.get('/bye', (req, res) => res.send('bye!'));
 
 app.listen(3000, () => console.log('server is up'));
