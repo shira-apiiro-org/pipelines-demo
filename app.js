1faa3775a863e275feef3dc1d1c4d60eb5d26189
Tomer Amir <tomer@apiiro.com>
2025-01-28 10:46:44 +0200

    add endpoint

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = require('express')();
 process.on('SIGINT', () => process.exit(0));
 
 app.get('/', (req, res) => res.send('success!'));
+app.get('/hi', (req, res) => res.send('hi!'));
 
 app.listen(3000, () => console.log('server is up'));
